3faa237e31e5f5f10f97ad2723ea121d5e25e04e
George Mamaladze <gmamaladze@gmail.com>
2016-08-19 00:16:52 +0200

    Mock ScratchExtensions

--- tests.js
+++ tests.js
@@ -1,3 +1,9 @@
+var ScratchExtensions;
+ScratchExtensions.register = function register(name, descriptor, ext) {
+    //Mock
+}
+
+
 QUnit.test( "hello test", function( assert ) {
   assert.ok( 1 == "1", "Passed!" );
 });
